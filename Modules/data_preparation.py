e517903c478d830bf38adcf86f4006eb8e3e2c02
NieThor <61249848+NieThor@users.noreply.github.com>
2020-06-09 17:21:49 +0200

    updated data preparation and main file

--- Modules/data_preparation.py
+++ Modules/data_preparation.py
@@ -2,5 +2,38 @@ import pandas as pd
 
 
 def resample_data(df: pd.DataFrame, resample_type=None):
-    resampled_data = df.copy()
-    return resampled_data
+    resampled_df = pd.DataFrame(data=None, columns=df.columns)
+    df['pct_change'] = df['close'].pct_change().abs()
+    resample_threshold = 0.02
+    last_value = 0
+    low_ = df['low'].iloc[1]
+    high_ = df['high'].iloc[1]
+    open_ = df['open'].iloc[1]
+    close_ = df['close'].iloc[1]
+    volume_ = 0
+    for i, row in enumerate(df.iterrows()):
+        row = row[1]
+        if i != 0:
+            curr_value = row['pct_change']
+            if last_value < resample_threshold:
+                curr_value += last_value
+                low_ = row['low'] if row['low'] < low_ else low_
+                high_ = row['high'] if row['high'] > high_ else high_
+                close_ = row['close']
+                volume_\
+                    += row['volume']
+            else:
+                resampled_df = resampled_df.append({'open': open_,
+                                                    'high': high_,
+                                                    'low': low_,
+                                                    'close': close_,
+                                                    'volume': volume_}, ignore_index=True)
+                low_ = row['low']
+                high_ = row['high']
+                open_ = row['open']
+                close_ = row['close']
+                volume_ = row['volume']
+
+            last_value = curr_value
+
+    return resampled_df
--- main.py
+++ main.py
@@ -6,20 +6,38 @@ from Modules.Machine_Learning_Module.prediction import predict
 from Modules.data_preparation import resample_data
 from Modules.indicators import apply_indicators
 
+import os
+
 
 def load_data():
-    df = pd.DataFrame()
-    return df
+    path = '.\\data\\binance'
+    data = {}
+    time_frame = '5m'
+    for file in os.listdir(path):
+        filename = os.fsdecode(file)
+        if filename.endswith(f'{time_frame}.json'):
+            ticker = filename.split(f'-{time_frame}')[0].replace('_', '/')
+            data[ticker] = pd.read_json(os.path.join(path, filename))
+            data[ticker].columns = ['time', 'open', 'high', 'low', 'close', 'volume']
+            data[ticker].set_index('time', inplace=True)
+            if len(data) > 1:
+                break
+            continue
+        else:
+            continue
+    return data
 
 
 def main():
-    df = load_data()
-    validation_df = df[-50:]
-    prepared_df = resample_data(df[:-50])
-    df = apply_indicators(prepared_df)
-    train, test = prepare_data(df)
-    models = fit_models(train, test)
-    predictions = predict(validation_df, models)
+    data = load_data()
+    validation_data = {key: df[-50:] for key, df in data.items()}
+    prepared_data = {key: resample_data(df[:-50]) for key, df in data.items()}
+    data = {key: apply_indicators(df) for key, df in prepared_data.items()}
+    train_data, test_data = {}, {}
+    for key, df in data.items():
+        train_data[key], test_data[key] = prepare_data(df)
+    models = {key: fit_models(train_data[key], test_data[key]) for key in train_data.keys()}
+    predictions = {key: predict(validation_df, models[key]) for key, validation_df in validation_data.items()}
     print(predictions)
 
 
